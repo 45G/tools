7680c099146e85aae50c9eeb38c276d79587a696
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2017-04-03 15:22:53 +0300

    fixed chunk size

--- tcp_ping.py
+++ tcp_ping.py
@@ -11,21 +11,28 @@ else:
 if len(sys.argv) <= 2:
     port = 1234
 else:
-    port = sys.argv[2]
+    port = int(sys.argv[2])
 
 if len(sys.argv) <= 3:
     psize = 1
 else:
     psize = int(sys.argv[3])
 
-s.connect((destination, 1234))
+# max 1700000 bytes for socat
+
+
+s.connect((destination, port))
 
 while 1:
     before = datetime.now()
-    s.send((time.ctime() + 'X' * psize +  '\n').encode())
-    data = s.recv(4096)
+    sbytes = s.send((time.ctime() + 'X' * psize +  '\n').encode())
+    data = s.recv(sbytes)
+    rbytes = len(data)
+    while rbytes < sbytes:
+        data = s.recv(sbytes - rbytes)
+        rbytes = rbytes + len(data)
     after = datetime.now()
     data = data.decode()
-    print (after-before).seconds*1000.0 + (after-before).microseconds/1000.0, "ms"
+    print sbytes, "bytes", (after-before).seconds*1000.0 + (after-before).microseconds/1000.0, " ms"
 
     time.sleep(0.2)
