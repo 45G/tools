3083ccbe45fc803b4fe7adf3074bebe3e08a689e
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2017-06-08 20:59:40 +0300

    reverse iperf; options for tcp_ping

--- tcp_ping.py
+++ tcp_ping.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python
 
-import time,sys,argparse 
+import time,sys,argparse,socket 
 from datetime import datetime
-from socket import *
+#from socket import *
+
+def filter_verbose (s, v):
+    if int(args.verbose) >= v:
+        return s
+    else:
+        return ""
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--length')
+    parser.add_argument('-l', '--length', default = 1)
     parser.add_argument('-tfo', dest='tfo', action='store_true')
+    parser.add_argument('-v', dest='verbose', default = 0)
+    parser.add_argument('-c', dest='count', default = 1)
+    parser.add_argument('-I', dest='interface')
+    parser.add_argument('-i', dest='interval', default = 1.0)
     parser.add_argument("destination")
     parser.add_argument("port")
     args = parser.parse_args()
-    print args
-    print args.tfo, args.length, args.destination, args.port
-
+    #print args
 
 destination = args.destination
 port = int(args.port) 
-psize = int(args.length)  
+psize = int(args.length) 
+count = int(args.count) 
+interface = args.interface
+interval = float(args.interval)
 # max 1700000 bytes for socat
 
-while 1:
-
+while count != 0:
+    count = count - 1
     before = datetime.now()
-    s = socket(AF_INET, SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if args.interface != None: 
+        s.setsockopt(socket.SOL_SOCKET, 25, args.interface + '\0')
     s.connect((destination, port))
     sbytes = s.send((time.ctime() + 'X' * psize +  '\n').encode())
     data = s.recv(sbytes)
@@ -35,8 +48,10 @@ while 1:
         #    print data.decode()
         rbytes = rbytes + len(data)
     after = datetime.now()
+    print filter_verbose ("src: " +  str(s.getsockname()[0]) + " port: " + str(s.getsockname()[1]), 1),\
+                          "bytes:", sbytes, "ms:", (after-before).seconds*1000.0 + (after-before).microseconds/1000.0
     s.close()
     data = data.decode()
-    print sbytes, "bytes", (after-before).seconds*1000.0 + (after-before).microseconds/1000.0, " ms"
 
-    time.sleep(0.2)
+    if count != 0:
+        time.sleep(interval)
