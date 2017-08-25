00e268a6fdbeac8a445603bf7c2ab2f472328687
Laura Cristina Ruse <laura.ruse@cs.pub.ro>
2017-08-25 17:31:13 +0300

    Parse and print statistics for ping output

---
+++ parse_data_ping.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+from math import sqrt
+from numpy import median
+from numpy import average
+from numpy import mean
+from numpy import std
+from numpy import histogram
+
+f = open(sys.argv[1])
+
+rtt = []
+
+for line in f:
+	data = line.split()
+	if (len(data) == 8):
+		data2 = data[6].split('=')
+		print data2[1]
+		rtt.append(float(data2[1]))
+
+print min(rtt), average(rtt), median(rtt), max(rtt), std(rtt),  "ms"
+print histogram(rtt)
+
+f.close()
